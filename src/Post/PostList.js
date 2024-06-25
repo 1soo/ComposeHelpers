a8b09354a480db2631b93d4243803f7906056f67
1soo <aaa4799@naver.com>
2024-06-25 14:22:37 +0900

    빈 배열 관련 오류 해결

--- src/Post/PostList.js
+++ src/Post/PostList.js
@@ -16,9 +16,12 @@ function PostList(props) {
 
     // postId값 초기화
     useEffect(()=>{
-        if(localStorage.getItem('postList')){
+        let postList = JSON.parse(localStorage.getItem('postList'));
+        if(postList != null && postList.length != 0){
             let postArray = JSON.parse(localStorage.getItem('postList'));
             setPostId(postArray[postArray.length-1].postId + 1);
+        }else{
+            localStorage.clear();
         }
     }, []);
 
