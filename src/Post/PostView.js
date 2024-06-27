24d17b0b8663ed48780a1daa92a906f1b36dc1f6
jnhyk <hjh130101@gmail.com>
2024-06-27 15:14:48 +0900

    부품 표로만

--- src/Post/PostView.js
+++ src/Post/PostView.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import CommentList from '../Comment/CommentList';
 
 function PostView(props, onAddComment){
@@ -9,10 +9,44 @@ function PostView(props, onAddComment){
     return(
         <div className="modal">
             <div className="viewContainer">
-                <h1 className="title">{selectpostList.title}</h1>
-                <div class="division-line"></div>
+                <div className="title">
+                    <h1 id="title">
+                        {selectpostList.title}
+                    </h1>
+                </div>
                 <div className="content">{selectpostList.content}</div>
-                <div className="parts">부품</div>
+                <div className="parts">
+                <table>
+                        <tr>
+                            <th>항목</th>
+                            <th>상세 정보</th>
+                        </tr>
+                        <tr>
+                            <td>CPU</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>메인보드</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>메모리</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>그래픽카드</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>SSD</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>파워</td>
+                            <td></td>
+                        </tr>
+                    </table>
+                </div>
                 {/* 댓글 */}
                 <div className="reply">
                     <CommentList postId={props.postId} onAddComment={onAddComment} />
